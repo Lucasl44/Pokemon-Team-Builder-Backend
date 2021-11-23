dfe3d88a67bf6034089122ea87b68bc3552037ff
Lucas Lederman <ljlederman@gmail.com>
2021-11-23 17:08:00 +0000

    Got adding routes sorted for database

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
+const { getAllPokemon, deleteDB, createDB } = require("../utils/pokemonDB");
 
 router.get("/", async (req, res) => {
-    res.status(201).json({ msg: "All Pokemon", data: await getAllPokemon()})
+    res.status(200).json({ msg: "All Pokemon", data: await getAllPokemon()})
+});
+
+router.post("/", async (req, res) => {
+    res.status(200).json({msg: "Added Pokemon", data: await createDB(req.body.pokemon)})
+});
+
+router.delete("/", async (req, res) => {
+    res.status(200).json({ msg: "Deleted Database", data: await deleteDB()})
 });
 
 module.exports = router;
--- utils/pokemonDB.js
+++ utils/pokemonDB.js
@@ -1,9 +1,21 @@
 const { Pokemon } = require("../models/pokemon");
 
 const getAllPokemon = async () => {
-    return "yes";
-}
+    return await Pokemon.findAll({});
+};
+
+const deleteDB = async () => {
+    return await Pokemon.destroy({where: {}})
+};
+
+const createDB = async (body) => {
+    for (let pokemon of body) {
+        await Pokemon.create(pokemon)
+    }
+};
 
 module.exports = {
-    getAllPokemon
+    getAllPokemon,
+    deleteDB,
+    createDB
 };
