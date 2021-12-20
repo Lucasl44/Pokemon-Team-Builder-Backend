eca222df1ab680e0be72c9a920e0ec864a18afd9
Lucas Lederman <ljlederman@gmail.com>
2021-12-20 11:58:43 +0000

    Closing in on type logic

--- utils/requests.js
+++ utils/requests.js
@@ -1,11 +1,52 @@
 const { Pokemon } = require("../models/pokemon");
+const {  Normal,
+    Fighting,
+    Flying,
+    Poison,
+    Ground,
+    Rock,
+    Bug,
+    Ghost,
+    Steel,
+    Fire,
+    Water,
+    Grass,
+    Electric,
+    Psychic,
+    Ice,
+    Dragon,
+    Dark,
+    Fairy } = require("../typeLogic");
+
+const mainTypes = [ "Normal", "Fighting", "Flying", "Poison", "Ground", "Rock", "Bug", "Ghost", "Steel", "Fire", "Water", "Grass", "Electric", "Psychic", "Ice", "Dragon", "Dark", "Fairy" ];
+
+const typeEffectiveness = [Normal, Fighting, Flying, Poison, Ground, Rock, Bug, Ghost, Steel, Fire, Water, Grass, Electric, Psychic, Ice, Dragon, Dark, Fairy]
+
+let types = [];
 
 const getTeam = async (body) => {
     let data = await Pokemon.findAll({ where: {pokemonName: [body]}});
     for (let i = 0; i < data.length; i++) {
-        console.log(data[i].dataValues);
+        types.push(data[i].dataValues.typeOne);
+        if (data[i].dataValues.typeTwo != null) {
+            types.push(data[i].dataValues.typeTwo);
+        }
+    };
+    let uniqueTypes = [... new Set(types)];
+    for (let i = 0; i < uniqueTypes.length; i++) {
+        for (let x = 0; x < typeEffectiveness.length; x++){
+            if (uniqueTypes[i] === mainTypes[x]){
+                let temp = typeEffectiveness[x];
+                for (let y = 0; y < typeEffectiveness.length; y++){
+                    console.log(temp[mainTypes[y]])
+                    //run it from here getting close
+                }
+        }
     }
+}
 
 };
+
+
 
 module.exports = { getTeam }; 
